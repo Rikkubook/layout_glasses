120aea2a3c0df94af9f00b7f5db253bf8b09259c
Rikku <rikkubook@gamil.com>
2020-05-10 17:53:49 +0800

    [visual 常見問題]

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,5 +1,15 @@
+//漢堡
+
 $('.hamburger').click(function(e) {
     console.log('a');
     $('.header__menu').toggleClass('header__menu--active');
     $('.hamburger').toggleClass('hamburger--active');
+});
+
+//手風琴
+$(".collapse__item h4").click(function() {
+    console.log("a");
+
+    $(this).parent().toggleClass("is-focus");
+
 });
