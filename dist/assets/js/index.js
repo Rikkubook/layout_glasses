bab219cf24bce39b1656c9f8cd5550cd77a62325
林佩瑤 <rikkubook@gmail.com>
2020-05-13 22:59:54 +0800

    [visual 我不是關紅線了嗎...]

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,15 +1,13 @@
 //漢堡
 
 $('.hamburger').click(function(e) {
-    console.log('a');
+    // console.log('a');
     $('.header__menu').toggleClass('header__menu--active');
     $('.hamburger').toggleClass('hamburger--active');
 });
 
 //手風琴
 $(".collapse__item h4").click(function() {
-    console.log("a");
-
+    // console.log("a");
     $(this).parent().toggleClass("is-focus");
-
 });
